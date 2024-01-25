7c3b778554f3b008d1bf7594cc83dcd6f3cf43ac
tiif <pekyuan@gmail.com>
2024-01-26 02:37:48 +0800

    Update

--- README.md
+++ README.md
@@ -1,3 +1,6 @@
 ### Lox intrepreter
 
 This repo contains the code and note for tutorial https://craftinginterpreters.com/
+
+
+*The note is beyond understandable and contains error. 
---
+++ com/craftinginterpreters/lox/AstPrinter.java
@@ -0,0 +1,58 @@
+package com.craftinginterpreters.lox;
+
+// AstPrinter implements the visitor interface, it is one of the operation(column)
+// Visit methods for each of the expression types is needed
+class AstPrinter implements Expr.Visitor<String> {
+  String print(Expr expr) {
+    return expr.accept(this);
+  }
+
+  @Override
+  public String visitBinaryExpr(Expr.Binary expr) {
+    return parenthesize(expr.operator.lexeme,
+                        expr.left, expr.right);
+  }
+
+  @Override
+  public String visitGroupingExpr(Expr.Grouping expr) {
+    return parenthesize("group", expr.expression);
+  }
+
+  @Override
+  public String visitLiteralExpr(Expr.Literal expr) {
+    if (expr.value == null) return "nil";
+    return expr.value.toString();
+  }
+
+  @Override
+  public String visitUnaryExpr(Expr.Unary expr) {
+    return parenthesize(expr.operator.lexeme, expr.right);
+  }
+
+  // This function produces a string with brackets, and includes all the expression inside
+  private String parenthesize(String name, Expr... exprs) {
+    StringBuilder builder = new StringBuilder();
+
+    builder.append("(").append(name);
+    for (Expr expr : exprs) {
+      builder.append(" ");
+      builder.append(expr.accept(this));
+    }
+    builder.append(")");
+
+    return builder.toString();
+  }
+
+  public static void main(String[] args) {
+    //manually input an expression
+    Expr expression = new Expr.Binary(
+        new Expr.Unary(
+            new Token(TokenType.MINUS, "-", null, 1),
+            new Expr.Literal(123)),
+        new Token(TokenType.STAR, "*", null, 1),
+        new Expr.Grouping(
+            new Expr.Literal(45.67)));
+
+    System.out.println(new AstPrinter().print(expression));
+  }
+}
